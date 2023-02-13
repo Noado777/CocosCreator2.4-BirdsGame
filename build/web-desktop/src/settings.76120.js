de6aa4fc7e94afbb8e39a28b9170f833ed44207a
Noado <97330956+Noado777@users.noreply.github.com>
2023-02-13 10:17:00 +0800

    Add files via upload

---
+++ build/web-desktop/src/settings.76120.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/start.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",resources:"97e76",main:"13eb0"}};
